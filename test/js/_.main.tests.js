8f1e0865aea44aef182a3a8e7964204c7f78fd98
Mohammed Arif <arif_iq@yahoo.co.in>
2012-06-06 19:09:22 +0530

    Removed the unused dir

--- test/js/_.main.tests.js
+++
@@ -1,8 +0,0 @@
-module("logger");
-
-test("Test logger utility - $.log()", function() {
-	expect(3);
-	ok( MODULE.config.debug, "MODULE.config.debug is defined" );
-	ok( window.console != 'undefined', "window.console is available" );
-	ok($.log, "$.log")
-});
