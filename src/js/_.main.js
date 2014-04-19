b901a5854c57497d1a25f265b1a78146ed2e1b13
Mohammed Arif <arif_iq@yahoo.co.in>
2014-04-19 16:31:26 +0530

    Formatted the main js alongwith with some very minor changes in the code

--- src/js/_.main.js
+++ src/js/_.main.js
@@ -11,44 +11,45 @@
  * mutable pre-ES5.
  */
 
-(function (MODULE, $, undefined) {
+(function(MODULE, $, undefined) {
     'use strict';
 
     /**
      * Logging function, for debugging mode
      */
-	$.log = function (message) {
+    $.log = function(message) {
         if (MODULE.config.debug && (typeof window.console !== 'undefined' && typeof window.console.log !== 'undefined') && console.debug) {
             console.debug(message);
-        } /*else {
+        }
+        /*else {
             alert(message);
         }*/
     };
 
-	/**
+    /**
      * Angus Croll awesome typeof fix from http://goo.gl/GtvsU
      */
-	$.toType = (function toType(global) {
-		return function (obj) {
-			if (obj === global) {
-				return 'global';
-			}
-			return ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1].toLowerCase();
-		};
-	}(this));
+    $.toType = (function toType(global) {
+        return function(obj) {
+            if (obj === global) {
+                return 'global';
+            }
+            return ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1].toLowerCase();
+        };
+    }(this));
 
-	/*$.toType(window); //'global' (all browsers)
+    /*$.toType(window); //'global' (all browsers)
 	$.toType([1,2,3]); //'array' (all browsers)
 	$.toType(/a-z/); //'regexp' (all browsers)
 	$.toType(JSON); //'json' (all browsers)
 	$.toType(null); //'null' (all browsers)
 	$.toType(undefined); //'undefined' (all browsers)*/
-	//etc..
+    //etc..
 
-	/**
+    /**
      * Private properties
      */
-    var name = 'Arif',
+    var name = 'Mohammed Arif',
         age = 30;
 
     /**
@@ -60,44 +61,46 @@
      * 2. It enforces good semicolon habits.
      * 3. Doesn't have much of the baggage traditionally associated with functions and scope.
      */
-    var getName = function () {
-        $.log('My name is ' + name + 'I am ' + age + ' old');
+    var getName = function() {
+        return 'My name is ' + name + ', I am ' + age + ' old.';
     };
 
-	/**
+
+    /**
      * Public methods and properties
      */
     MODULE.title = 'Interactive Developer';
-    MODULE.sayHello = function () {
-        $.log('hello world');
+    MODULE.sayHello = function() {
+        return "Hello World!"
     };
 
-	/*
+    /*
      * Singletons serve as a namespace provider which isolate implementation code
      * from the global namespace so as to provide a single point of access for functions,
      * this is useful for organizing code into logical sections.
      * It is possible to put parentheses around this structure to instantiate it immediately after it's parsed.
      * This way it's always present when the script is executed and doesn't have to be instantiated separately.
-	*/
-    MODULE.subModule = (function () {
+     */
+    MODULE.subModule = (function() {
         function _subModule() {
 
             /**
-            * In non-strict mode, 'this' is bound to the global scope when it isn't bound to anything else.
-            * In strict mode it is 'undefined'. That makes it an error to use it outside of a method.
-            */
+             * In non-strict mode, 'this' is bound to the global scope when it isn't bound to anything else.
+             * In strict mode it is 'undefined'. That makes it an error to use it outside of a method.
+             */
 
             /*jshint validthis: true */
             var _this = this;
             /* Store this to avoid scope conflicts */
 
-            this.fbReady = function () {
+            this.fbReady = function() {
                 /**
                  * This code loads the SDK asynchronously so it does not block loading other elements of your page. This is particularly important to ensure fast page loads for users and SEO robots.
                  */
-                window.fbAsyncInit = function () {
+                window.fbAsyncInit = function() {
                     FB.init({
-                        appId: MODULE.config.appId, /*read from config*/
+                        appId: MODULE.config.appId,
+                        /*read from config*/
                         status: true,
                         cookie: true,
                         xfbml: true
@@ -106,7 +109,7 @@
                     /**
                      * The FB.Event.subscribe is used to subscribe to login events
                      */
-                    FB.Event.subscribe('auth.login', function () {
+                    FB.Event.subscribe('auth.login', function() {
                         // do something when user logs in
                         //_this.login();
                     });
@@ -114,7 +117,7 @@
                     /**
                      * The FB.Event.subscribe is used to subscribe to logout events
                      */
-                    FB.Event.subscribe('auth.logout', function () {
+                    FB.Event.subscribe('auth.logout', function() {
                         // do something when user logs out.
                         //_this.logout();
                     });
@@ -122,7 +125,7 @@
                     /**
                      * To determine if a user is connected to your app
                      */
-                    FB.getLoginStatus(function (response) {
+                    FB.getLoginStatus(function(response) {
                         if (response.status === 'connected') {
                             // the user is logged in and connected to your
                             // app, and response.authResponse supplies
@@ -141,24 +144,23 @@
                 /**
                  * Load the SDK Asynchronously
                  */
-                (function (d) {
-                    var js, id = 'facebook-jssdk';
+                (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
                     if (d.getElementById(id)) {
                         return;
                     }
-                    js = d.createElement('script');
+                    js = d.createElement(s);
                     js.id = id;
-                    js.async = true;
-                    js.src = '//connect.facebook.net/en_US/all.js';
-                    d.getElementsByTagName('head')[0].appendChild(js);
-                }(document));
+                    js.src = "//connect.facebook.net/en_US/all.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
             };
 
             /**
              * facebookLogin - FB.login prompts the user to authorize your application
              */
-            this.facebookLogin = function () {
-                FB.login(function (response) {
+            this.facebookLogin = function() {
+                FB.login(function(response) {
                     if (response.status === 'connected') {
                         $.log('User is logged in and granted some permissions.');
                     } else if ((response.status === 'not_authorized') || response.authResponse === null) {
@@ -172,7 +174,7 @@
             /**
              * Init call
              */
-            this.init = function () {
+            this.init = function() {
                 _this.fbReady();
                 return this; /*this refer to MODULE.subModule*/
             };
@@ -182,7 +184,7 @@
         return new _subModule(); /*creating a new object of subModule rather then a funtion*/
     }());
 
-/**
- * Check to evaluate whether 'MODULE' exists in the global namespace - if not, assign window.MODULE an object literal
- */
+    /**
+     * Check to evaluate whether 'MODULE' exists in the global namespace - if not, assign window.MODULE an object literal
+     */
 }(window.MODULE = window.MODULE || {}, jQuery));
