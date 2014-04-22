37a055e0e64e6b3569f28bfac9be9443ba019e8b
Mohammed Arif <arif_iq@yahoo.co.in>
2014-04-22 11:43:05 +0530

    Have updated the test cases as well w.r.t module changed

--- test/unit/_.mainSpec.js
+++ test/unit/_.mainSpec.js
@@ -49,22 +49,22 @@ describe("Test main js file", function() {
 	 */
 	describe("test log function", function() { // suite
 
-		it("MODULE.config.debug should be true", function() { // specification or spec
-			expect(MODULE.config.debug).toBe(true); // matcher, if you want to make sure something is literally true or false and nothing else, use the
+		it("JSB.config.debug should be true", function() { // specification or spec
+			expect(JSB.config.debug).toBe(true); // matcher, if you want to make sure something is literally true or false and nothing else, use the
 			//toEqual matcher
 		});
 
 		it("typeof window.console should be object", function() {
 			expect(typeof window.console !== 'undefined').toEqual(true);
-		})
+		});
 
 		it("typeof window.console.log to be defined", function() {
 			expect(typeof window.console.log !== 'undefined').toEqual(true);
-		})
+		});
 
 		it("console.debug should be available", function() {
 			expect(console.debug).toBeDefined();
-		})
+		});
 	});
 
 	/**
@@ -73,85 +73,85 @@ describe("Test main js file", function() {
 	describe("test toType function", function() {
 		xit("test window object 'global'", function() {
 			expect($.toType(window)).toEqual("global");
-		})
+		});
 
 		it("test array object", function() {
 			expect($.toType([1, 2, 3])).toEqual("array");
-		})
+		});
 
 		it("test regex object", function() {
 			expect($.toType(/a-z/)).toEqual("regexp");
-		})
+		});
 
 		it("test object", function() {
 			expect($.toType({
 				a: 4
 			})).toEqual("object");
-		})
+		});
 
 		it("test error object", function() {
 			expect($.toType(new ReferenceError)).toEqual("error");
-		})
+		});
 
 		it("test date object", function() {
 			expect($.toType(new Date)).toEqual("date");
-		})
+		});
 
 		it("test Math object", function() {
 			expect($.toType(Math)).toEqual("math");
-		})
+		});
 
 		it("test JSON object", function() {
 			expect($.toType(JSON)).toEqual("json");
-		})
+		});
 
 		it("test Number object", function() {
 			expect($.toType(3)).toEqual("number");
-		})
+		});
 
 		it("test String object", function() {
 			expect($.toType("Arif")).toEqual("string");
-		})
+		});
 
 		it("test Boolean object", function() {
 			expect($.toType(new Boolean(true))).toEqual("boolean");
-		})
+		});
 
 		xit("test null", function() {
 			expect($.toType(null)).toEqual("null");
-		})
+		});
 
 		xit("test undefined", function() {
 			expect($.toType(undefined)).toEqual("undefined");
-		})
-	})
-
-	describe("test public method", function() {
-		it("MODULE.sayHello", function() {
-			expect(MODULE.sayHello()).toEqual("Hello World!");
-		})
-	})
-
-
-	describe("Facebook", function() {
-		it("is FB available (i.e. is facebook library included)", function() {
-			expect(typeof FB != "undefined").not.toEqual(true);
-		});
-
-		it("do we have an application id", function() {
-			expect(MODULE.config.appId != "").toEqual(true);
-		});
-
-		it("initialize the facebook library", function() {
-			MODULE.subModule.fbReady();
-		});
-
-		xit("Checking for active facebook session", function() {
-			FB.getLoginStatus();
-			waitsFor(function() {
-				return FB.getLoginStatusResponse() && FB.getLoginStatusResponse().authResponse;
-			}, "response.authResponse", 7000);
 		});
 	});
+
+	describe("test public method", function() {
+		it("JSB.sayHello", function() {
+			expect(JSB.sayHello()).toEqual("Hello World!");
+		});
+	});
+
+
+	// describe("Facebook", function() {
+	// 	it("is FB available (i.e. is facebook library included)", function() {
+	// 		expect(typeof FB != "undefined").not.toEqual(true);
+	// 	});
+
+	// 	it("do we have an application id", function() {
+	// 		expect(JSB.config.appId != "").toEqual(true);
+	// 	});
+
+	// 	it("initialize the facebook library", function() {
+	// 		JSB.subModule.fbReady();
+	// 	});
+
+	// 	xit("Checking for active facebook session", function() {
+	// 		FB.getLoginStatus();
+	// 		waitsFor(function() {
+	// 			return FB.getLoginStatusResponse() && FB.getLoginStatusResponse().authResponse;
+	// 		}, "response.authResponse", 7000);
+	// 	});
+	// });
 
 })
