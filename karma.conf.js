684d693ee818f02528a4cd7f0e4fd1690c3ce6b6
Mohammed Arif <arif_iq@yahoo.co.in>
2015-07-01 19:22:43 +0530

    Changed the karma configuration as per the coverage requirement.

--- karma.conf.js
+++ karma.conf.js
@@ -1,85 +1,93 @@
 module.exports = function(config) {
-    config.set({
+	config.set({
 
-        // base path, that will be used to resolve files and exclude
-        basePath: '',
+		// base path, that will be used to resolve files and exclude
+		basePath: '',
 
-        // list of files/patterns to load in the browser
-        files: [
-            'src/js/libs/jquery.js',
-            'src/js/*.js',
-            'test/unit/*.js'
-        ],
+		// list of files/patterns to load in the browser
+		files: [
+			'src/js/libs/jquery.js',
+			'src/js/*.js',
+			'test/unit/*.js'
+		],
 
-        // preprocessors: {
-        //     '**/src/js/**/*.js': 'coverage'
-        // },
+		// preprocessors: {
+		//     '**/src/js/**/*.js': 'coverage'
+		// },
 
-        // list of files to exclude
-        exclude: [],
+		// list of files to exclude
+		exclude: [],
 
-        // use dolts reporter, as travis terminal does not support escaping sequences
-        // possible values: 'dots', 'progress', 'junit', 'teamcity'
-        // CLI --reporters progress
-        reporters: ['progress'],
+		// use dolts reporter, as travis terminal does not support escaping sequences
+		// possible values: 'dots', 'progress', 'junit', 'teamcity'
+		// CLI --reporters progress
+		reporters: ['progress', 'coverage'],
 
-        coverageReporter: {
-            type: 'html',
-            dir: 'test/coverage/'
-        },
+		preprocessors: { //The preprocessor configures which files should be tested for coverage. For example if all your code lives in lib/ you need to add this to your configuration file.
+			// source files, that you wanna generate coverage for
+			// do not include tests or libraries
+			// (these files will be instrumented by Istanbul)
+			'src/js/*.js': ['coverage']
+		},
 
-        // web server port
-        // CLI --port 9876
-        port: 9876,
+		coverageReporter: {
+			type: 'html',
+			dir: 'test/coverage/'
+		},
 
-        // cli runner port
-        // CLI --runner-port 9100
-        runnerPort: 9100,
+		// web server port
+		// CLI --port 9876
+		port: 9876,
 
-        // enable / disable colors in the output (reporters and logs)
-        // CLI --colors --no-colors
-        colors: true,
+		// cli runner port
+		// CLI --runner-port 9100
+		runnerPort: 9100,
 
-        // level of logging
-        logLevel: config.LOG_INFO,
+		// enable / disable colors in the output (reporters and logs)
+		// CLI --colors --no-colors
+		colors: true,
 
-        // enable / disable watching file and executing tests whenever any file changes
-        // CLI --auto-watch --no-auto-watch
-        autoWatch: true,
+		// level of logging
+		logLevel: config.LOG_ERROR,
 
-        // load jasmine (this replaces JASMINE and JASMINE_ADAPTER file references)
-        frameworks: ['jasmine'],
+		// enable / disable watching file and executing tests whenever any file changes
+		// CLI --auto-watch --no-auto-watch
+		autoWatch: true,
 
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera
-        // - Safari (only Mac)
-        // - PhantomJS
-        // - IE (only Windows)
-        // CLI --browsers Chrome,Firefox,Safari
-        browsers: ['PhantomJS'],
-        //browsers: ['Chrome'],
+		// load jasmine (this replaces JASMINE and JASMINE_ADAPTER file references)
+		frameworks: ['jasmine'],
 
-        // If browser does not capture in given timeout [ms], kill it
-        // CLI --capture-timeout 5000
-        captureTimeout: 60000,
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		// CLI --browsers Chrome,Firefox,Safari
+		browsers: ['PhantomJS'],
+		//browsers: ['Chrome'],
 
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: false,
+		// If browser does not capture in given timeout [ms], kill it
+		// CLI --capture-timeout 5000
+		captureTimeout: 60000,
 
-        // report which specs are slower than 500ms
-        // CLI --report-slower-than 500
-        reportSlowerThan: 500,
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun: false,
 
-        // load the needed plugins (according to karma docs, this should not be needed tho)
-        plugins: [
-            'karma-jasmine',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-phantomjs-launcher'
-        ]
-    });
+		// report which specs are slower than 500ms
+		// CLI --report-slower-than 500
+		reportSlowerThan: 500,
+
+		// load the needed plugins (according to karma docs, this should not be needed tho)
+		plugins: [
+			'karma-jasmine',
+			'karma-coverage',
+			'karma-chrome-launcher',
+			'karma-firefox-launcher',
+			'karma-phantomjs-launcher'
+		]
+	});
 };
